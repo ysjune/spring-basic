3eb68bd7696816f2e92babbf028883f48eb32b35
ysjune <seokjuneyeom@gmail.com>
2022-03-13 18:45:49 +0900

    feat(spring container and bean): chapter spring container and spring bean

---
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,20 @@
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+
+   AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String name : beanDefinitionNames) {
+            ac.getBeanDefinition(name);
+        }
+    }
+}
